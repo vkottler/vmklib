2c1acbb5e37d8d04d0dc4fde5a360cd3ea2b55c6
Vaughn Kottler <vaughnkottler@gmail.com>
2020-12-09 00:47:10 -0800

    inc minor version since we fixed a bug

--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=1.1.1
-# hash=f9a15b5259d44524ed4f33a1a4c71506
+# hash=8a4c31824076f298fcbf469a4bbc2a5f
 # =====================================
 
 """
@@ -9,5 +9,5 @@ vmklib - Useful defaults and other package metadata.
 """
 
 PKG_NAME = "vmklib"
-VERSION = "0.2.3"
+VERSION = "0.3.0"
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
