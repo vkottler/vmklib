3a6a5a743da06c60a1e8cfc83611ccdb5c46499a
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2022-03-22 00:01:01 -0700

    Add MK_CONC target

--- vmklib/data/vmklib.mk
+++ vmklib/data/vmklib.mk
@@ -1,11 +1,19 @@
 MK_PREFIX := mk-
+MK_INSTALL := $(MK_PREFIX)install
 
 .PHONY: $(MK_PREFIX)upgrade $(MK_PREFIX)sys-upgrade $(MK_PREFIX)header \
-        $(MK_PREFIX)todo
+        $(MK_PREFIX)todo $(MK_INSTALL)
 
 UPGRADE_CMD := install --upgrade vmklib
 
-$(MK_PREFIX)upgrade: $(VENV_CONC)
+MK_CONC := $(call to_concrete, $(MK_INSTALL)-$(VENV_NAME))
+$(MK_CONC): | $(VENV_CONC)
+	$(PIP) $(UPGRADE_CMD)
+	$(call generic_concrete,$@)
+
+$(MK_INSTALL): $(DZ_CONC)
+
+$(MK_PREFIX)upgrade: | $(VENV_CONC)
 	$(PIP) $(UPGRADE_CMD)
 
 $(MK_PREFIX)sys-upgrade:
