410e9ef37fa8da50f6e6b83a928e77addead5990
Vaughn Kottler <vaughnkottler@gmail.com>
2022-03-27 16:23:04 -0700

    0.8.2 - Roll changes into release

--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=1.14.0
-# hash=de2765044be8b5010d13e1ef1df54259
+# hash=63f1dc62b4961da9fd1e9e2ee5da753b
 # =====================================
 
 """
@@ -10,4 +10,4 @@ vmklib - Useful defaults and other package metadata.
 
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
 PKG_NAME = "vmklib"
-VERSION = "0.8.1"
+VERSION = "0.8.2"
