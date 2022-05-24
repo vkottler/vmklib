f7979a042a530abfee593245b753b162fe8259df
Vaughn Kottler <vaughnkottler@gmail.com>
2022-05-24 02:53:27 -0700

    1.4.0 - Implement python-lint as Python

--- vmklib/data/tasks/conf.py
+++ vmklib/data/tasks/conf.py
@@ -7,12 +7,21 @@ from pathlib import Path
 from typing import Dict
 
 # third-party
+from vcorelib.task import Inbox, Outbox, Task
 from vcorelib.task.dict.melder import DictMerger
 from vcorelib.task.manager import TaskManager
 
 # internal
 from vmklib.tasks.python import venv_bin, venv_dir
+from vmklib.tasks.python.lint import register as register_python_lint
 from vmklib.tasks.venv import register as register_venv
+
+
+class FailTask(Task):
+    """A task that always fails."""
+
+    async def run(self, inbox: Inbox, outbox: Outbox, *args, **kwargs) -> bool:
+        return False
 
 
 def register(
@@ -35,12 +44,15 @@ def register(
         "__files__": {"python": venv_bin(cwd, "python")},
     }
 
+    # Register a task that would always fail for testing.
+    manager.register(FailTask("fail"))
+
     # Register the initialization task.
     manager.register(DictMerger("vmklib.init", init_data, substitutions))
 
     # Load additional modules.
     result = True
-    for dep in [register_venv]:
+    for dep in [register_venv, register_python_lint]:
         if result:
             result = dep(manager, project, cwd, substitutions)
 
