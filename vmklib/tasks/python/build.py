b185e842f61904206f22b91a82b15e9d81a69f9b
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2022-06-03 17:19:49 -0700

    Some progress

--- vmklib/tasks/python/build.py
+++ vmklib/tasks/python/build.py
@@ -8,6 +8,24 @@ from typing import Dict
 
 # third-party
 from vcorelib.task.manager import TaskManager
+from vcorelib.task.subprocess.run import SubprocessLogMixin
+
+
+class PythonBuild(SubprocessLogMixin):
+    """Build a Python package."""
+
+    # Clean build, remove:
+    # * $($(PROJ)_DIR)/dist
+    # * $(BUILD_DIR)/bdist*
+    # * $(BUILD_DIR)/lib
+
+    # Build package:
+    # 'python -m build' inside the project directory
+
+    # for changing directories, we need some kind of concurrency safety
+    # probably
+
+    # Allow a 'build-once' target variant
 
 
 def register(
@@ -18,7 +36,13 @@ def register(
 ) -> bool:
     """Register package building tasks to the manager."""
 
-    del manager
+    # Make sure 'wheel' is also installed so we can build a wheel.
+    reqs = ["venv", "python-install-wheel", "python-install-build"]
+
+    manager.register(PythonBuild("python-build"), reqs)
+
+    manager.register(PythonBuild("python-build-once", once=True), reqs)
+
     del project
     del cwd
     del substitutions
