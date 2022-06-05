747c17341836a6d52855b47297ab90180d3ef16f
Vaughn Kottler <vaughnkottler@gmail.com>
2022-06-04 20:43:30 -0700

    1.5.0 - Attempt to enable Windows CI
    Removes Python 3.7 as a supported version.

--- tasks/conf.py
+++ tasks/conf.py
@@ -9,6 +9,7 @@ from typing import Dict
 # third-party
 from vcorelib.task import Phony
 from vcorelib.task.manager import TaskManager
+from vcorelib.task.subprocess.run import is_windows
 
 
 def register(
@@ -19,8 +20,10 @@ def register(
 ) -> bool:
     """Register project tasks to the manager."""
 
+    # Don't run yamllint on Windows because it will fail on newlines.
     manager.register(
-        Phony("yaml"), ["yaml-lint-local", "yaml-lint-manifest.yaml"]
+        Phony("yaml"),
+        [] if is_windows() else ["yaml-lint-local", "yaml-lint-manifest.yaml"],
     )
     del project
     del cwd
--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=2.0.0
-# hash=facc303ad4f0e8efeca737ea0854108b
+# hash=6e5f354845a51cfe63a58fe0e947d9c4
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
 PKG_NAME = "vmklib"
-VERSION = "1.4.4"
+VERSION = "1.5.0"
