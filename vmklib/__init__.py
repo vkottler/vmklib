90824ca3a028d22842bcbe73ba3912f8fb826ab8
Vaughn Kottler <vaughnkottler@gmail.com>
2023-07-13 15:58:35 -0500

    1.10.0 - Bump version

--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.2
-# hash=d7449a443b6b987d60bcbde5231ab563
+# hash=0e50415f07e485c70acde31abb963f05
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
 PKG_NAME = "vmklib"
-VERSION = "1.9.0"
+VERSION = "1.10.0"
