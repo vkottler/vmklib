e7b505a3a90d91893b84dde9c0dfd6de5456f574
Vaughn Kottler <vaughnkottler@gmail.com>
2023-07-22 15:24:10 -0500

    1.10.2 - Remove some vcorelib methods

--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.2
-# hash=089d813c9238c490a893adefbd7ad945
+# hash=968ffb78bb91f14f331f14edd76f9321
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
 PKG_NAME = "vmklib"
-VERSION = "1.10.1"
+VERSION = "1.10.2"
