b5029832cbda23d1386ee836894891ea9b5ecb8c
Vaughn Kottler <vaughnkottler@gmail.com>
2023-08-03 00:26:41 -0500

    1.10.4 - Disable some Windows linting

--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.2
-# hash=088bb07ad9cbed56c9ff6946182d6ef9
+# hash=85cd3a6aa7fb306ea62529e095aff22a
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
 PKG_NAME = "vmklib"
-VERSION = "1.10.3"
+VERSION = "1.10.4"
