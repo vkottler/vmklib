652981d67dc4d10e65f6ba91224b7fec57ada69e
Vaughn Kottler <vaughnkottler@gmail.com>
2024-05-17 16:09:47 -0700

    2.0.1 - Fix ruff warning

--- vmklib/__init__.py
+++ vmklib/__init__.py
@@ -1,7 +1,7 @@
 # =====================================
 # generator=datazen
 # version=3.1.4
-# hash=323a37ac953da26ff2b3ae5654e60ac6
+# hash=0b06924a9490f122f646f491d43e5fd6
 # =====================================
 
 """
@@ -10,4 +10,4 @@ Useful defaults and other package metadata.
 
 DESCRIPTION = "Simplify project workflows by standardizing use of GNU Make."
 PKG_NAME = "vmklib"
-VERSION = "2.0.0"
+VERSION = "2.0.1"
